21d2f2d76fde8c56dd6f6d9d4842dedb131a7867
Jimena <jparatge@gmail.com>
2024-11-03 20:45:07 +0000

    casi

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -13,7 +13,11 @@ const getState = ({ getStore, getActions, setStore }) => {
 					initial: "white"
 				}
 			],
-			contacts: [],
+			
+			contacts: [
+
+			],
+			
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
@@ -39,25 +43,75 @@ const getState = ({ getStore, getActions, setStore }) => {
 				//reset the global store
 				setStore({ demo: demo });
 			},
-
-
-			getContacts: async() => {
-				const resp = await fetch(process.env.BACKEND_URL+'agendas/jime');
-				const data = await resp.json()
+			//función para importar contacto
+			getContacts: async () => {
+				const resp = await fetch(process.env.BACKEND_URL+`agendas/ademontel`);
+				if(resp.status == 404){
+					await getActions().createAgenda()  // Se crea la nueva agenda usando el método Actions
+					return null
+				}
+				const data = await resp.json();
 				console.log(data);
 				setStore({contacts: data.contacts})
 			},
+			//Función para crear un contacto.
 			createContact: async (newContact) => {
 				const myHeaders = new Headers();
 				myHeaders.append("Content-Type", "application/json");
-				const resp = await fetch(process.env.BACKEND_URL+'agendas/jime/contacts', {
+				const resp = await fetch(process.env.BACKEND_URL+`agendas/ademontel/contacts`, {
 					method: 'POST',
-					headers: myHeaders, 
+					headers: myHeaders,
 					body: JSON.stringify(newContact),
 				});
-				if(resp.ok) { 
+				if(resp.ok) {
 					await getActions().getContacts()
 				}
+			},
+			 createAgenda: async () => {
+				try {
+					const myHeaders = new Headers();
+					myHeaders.append("Content-Type", "application/json");
+					const resp = await fetch(process.env.BACKEND_URL+`agendas/ademontel`, {
+						method: "POST",
+						headers: { "Content-Type": "application/json" }
+					})
+					if (resp.status == 201) {
+						await getActions().getContacts()
+					}
+				} catch (error) {
+					console.log(error)
+					return false
+				}
+			},
+			 deleteContact: async (contact_id) => {
+				const resp = await fetch(process.env.BACKEND_URL+`agendas/ademontel/contacts/${contact_id}`, {
+					method: "DELETE",
+				});
+		
+				if (resp.ok) {
+					await getActions().getContacts()
+				} else {
+					console.error("Error al eliminar la tarea");
+				}
+			},
+			editContact: async (id, updatedContact) => {
+				const myHeaders = new Headers();
+				myHeaders.append("Content-Type", "application/json");
+			
+				const resp = await fetch(`${process.env.BACKEND_URL}agendas/ademontel/contacts/${id}`, {
+					method: "PUT",
+					headers: myHeaders,
+					body: JSON.stringify(updatedContact),
+				});
+			
+				if (resp.ok) {
+					await getActions().getContacts(); // Actualiza la lista de contactos
+				} else {
+					console.error("Error al editar el contacto");
+				}
+			},
+			clearContact: () => {
+				setStore({ contact: { name: "", email: "", phone: "", address: "" } });
 			}
 		}
 	};
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,22 +1,20 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
-import { useContext, useState, useEffect } from "react";
-import { Context } from "../store/appContext.js";
+import { useContext, useEffect, useState } from "react";
+import {Context} from "../store/appContext.js";
 import { ContactCard } from "../component/card.js";
-
-
 
 export const Home = () => {
 
 	const {actions, store} = useContext(Context)
+
 	return (
-		<div className="text-center mt-5">
-			{
-				store.contacts.map((item,index) =>{
-					return <ContactCard key={index} name= {item.name} />
-				})
-			}
-		</div>
-	);
-};	
+	<div className="text-center mt-5">
+		{
+			store.contacts.map((item, index) =>{
+				/*console.log(item);*/
+				return <ContactCard key={index} name={item.name} id={item.id} address={item.address} email={item.email} phone={item.phone} />
+			})
+		}
+	</div>
+)};
